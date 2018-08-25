5dc75584b207cc677c65f9c96259afd495ef5e48
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-25 00:28:37 -0300

    Update Lista3_22.py

--- Lista3_22.py
+++ Lista3_22.py
@@ -1,3 +1,5 @@
+#Altere o programa de cálculo dos números primos, informando, caso o número não seja primo, por quais número ele é divisível.
+
 numero = int(input("\nDigite um número: "))
 lista = []
 

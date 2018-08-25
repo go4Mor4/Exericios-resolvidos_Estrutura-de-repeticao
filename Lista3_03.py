de65dd870a8e1dee2961a30049aee5a685a38550
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-25 00:21:40 -0300

    Update Lista3_03.py

--- Lista3_03.py
+++ Lista3_03.py
@@ -1,4 +1,11 @@
-#Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.
+'''
+Faça um programa que leia e valide as seguintes informações:
+Nome: maior que 3 caracteres;
+Idade: entre 0 e 150;
+Salário: maior que zero;
+Sexo: 'f' ou 'm';
+Estado Civil: 's', 'c', 'v', 'd';
+'''
 
 nome = input("Digite o seu nome: ")
 while len(nome) <= 3:

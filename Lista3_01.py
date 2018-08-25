4e4646e5cd0259765a18c2da94ee4333d0bea7d9
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-25 00:19:25 -0300

    Update Lista3_01.py

--- Lista3_01.py
+++ Lista3_01.py
@@ -1,3 +1,5 @@
+#Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
+
 nota = float(input("Digite uma nota entre zero e 10: "))
 
 while nota > 10 or nota < 0:

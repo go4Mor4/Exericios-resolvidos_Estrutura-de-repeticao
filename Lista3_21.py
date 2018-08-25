fae01e254892d956e9924a10a8784bd529f96850
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-25 00:28:17 -0300

    Update Lista3_21.py

--- Lista3_21.py
+++ Lista3_21.py
@@ -1,3 +1,5 @@
+#Faça um programa que peça um número inteiro e determine se ele é ou não um número primo. Um número primo é aquele que é divisível somente por ele mesmo e por 1.
+
 numero = int(input("\nDigite um número: "))
 
 if numero % 2 == 0 and numero != 2:

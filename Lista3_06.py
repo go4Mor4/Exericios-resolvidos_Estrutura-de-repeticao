41a28ba89bd2186f9403bf3fa4dafc2a4b2cc2f2
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-25 00:23:27 -0300

    Update Lista3_06.py

--- Lista3_06.py
+++ Lista3_06.py
@@ -1,3 +1,5 @@
+#Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois modifique o programa para que ele mostre os números um ao lado do outro.
+
 for i in range(1, 21):
     print(i)
 

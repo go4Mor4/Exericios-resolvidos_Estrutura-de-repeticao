26ec4198f096144bc177b95697c089d1d8753315
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-25 00:25:32 -0300

    Update Lista3_12.py

--- Lista3_12.py
+++ Lista3_12.py
@@ -1,3 +1,12 @@
+'''
+Desenvolva um gerador de tabuada, capaz de gerar a tabuada de qualquer número inteiro entre 1 a 10. O usuário deve informar de qual numero ele deseja ver a tabuada. A saída deve ser conforme o exemplo abaixo:
+Tabuada de 5:
+5 X 1 = 5
+5 X 2 = 10
+...
+5 X 10 = 50
+'''
+
 n = int(input("digite um numero de 1 a 10: "))
 cont = 1
 

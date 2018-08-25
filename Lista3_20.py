5305448c29ee31faae95a23b7b8731ea57e2a5dc
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-25 00:27:59 -0300

    Update Lista3_20.py

--- Lista3_20.py
+++ Lista3_20.py
@@ -1,3 +1,5 @@
+#Altere o programa de cálculo do fatorial, permitindo ao usuário calcular o fatorial várias vezes e limitando o fatorial a números inteiros positivos e menores que 16.
+
 import math
 lista = []
 count = 0

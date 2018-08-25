f8840fbe3097178cc3c370c09dc4442355261b25
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-25 00:24:50 -0300

    Update Lista3_10.py

--- Lista3_10.py
+++ Lista3_10.py
@@ -1,3 +1,5 @@
+#Faça um programa que receba dois números inteiros e gere os números inteiros que estão no intervalo compreendido por eles.
+
 n1 = int(input("Digite um número: "))
 n2 = int(input("Digite outro número: "))
 

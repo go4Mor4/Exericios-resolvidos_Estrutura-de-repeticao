59297d0acaaf990e3c7aba4542373f5f0bb1e107
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-25 00:19:51 -0300

    Update Lista3_02.py

--- Lista3_02.py
+++ Lista3_02.py
@@ -1,3 +1,5 @@
+#Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.
+
 nome = input("Digite o seu nome de usuario: ")
 senha = input("Digite a sua senha: ")
 

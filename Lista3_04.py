ba1b08fe7dfdf89a9f7d91266c3bbabdcbf5fc44
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-25 00:22:37 -0300

    Update Lista3_04.py

--- Lista3_04.py
+++ Lista3_04.py
@@ -1,3 +1,6 @@
+#Supondo que a população de um país A seja da ordem de 80000 habitantes com uma taxa anual de crescimento de 3% e que a população de B seja 200000 habitantes com uma taxa de crescimento de 1.5%. Faça um programa que calcule e escreva o número de anos necessários para que a população do país A ultrapasse ou iguale a população do país B, mantidas as taxas de crescimento.
+
+
 a = 80000
 b = 200000
 ano = 0

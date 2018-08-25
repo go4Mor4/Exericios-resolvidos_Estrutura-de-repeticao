42c8051d1b62d0433023c8782c44161ae492ab59
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-25 00:26:49 -0300

    Update Lista3_16.py

--- Lista3_16.py
+++ Lista3_16.py
@@ -1,3 +1,5 @@
+#A série de Fibonacci é formada pela seqüência 0,1,1,2,3,5,8,13,21,34,55,... Faça um programa que gere a série até que o valor seja maior que 500.
+
 ultimo = 1
 penultimo = 1
 print(ultimo)

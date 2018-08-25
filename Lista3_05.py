a76bbe3ad9b2b628ff730cdd9118d6eb7ab048ff
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-25 00:23:04 -0300

    Update Lista3_05.py

--- Lista3_05.py
+++ Lista3_05.py
@@ -1,3 +1,5 @@
+#Altere o programa anterior permitindo ao usuário informar as populações e as taxas de crescimento iniciais. Valide a entrada e permita repetir a operação.
+
 populacao1 = int(input("Digite a população 1: "))
 populacao2 = int(input("Digite a população 2: "))
 crescimento1 = input("Porcentagem de crescimento anual da população 1: ")

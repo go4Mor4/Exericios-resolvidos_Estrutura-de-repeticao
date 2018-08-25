57f7449e4e526701b40995fab3391646787fdfc4
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-25 00:25:50 -0300

    Update Lista3_13.py

--- Lista3_13.py
+++ Lista3_13.py
@@ -1,3 +1,5 @@
+#Faça um programa que peça dois números, base e expoente, calcule e mostre o primeiro número elevado ao segundo número. Não utilize a função de potência da linguagem.
+
 base = int(input("Digite a base: "))
 expoente = int(input("Digite expoente: "))
 resultado = 1

b4dc722bf66cba96064fd0c7e2d33e10edb4ec2e
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-09-14 12:48:04 -0300

    Delete 43.py

--- 43.py
+++
@@ -1,9 +0,0 @@
-menu = [item1[100, 'Cachorro Quente', 1.20], item2[101, 'Bauru Simples', 1.30], item3[102, 'Bauru Com Ovo', 1.50], item4[103, 'Hamburguer', 1.20], item5[104, 'ChesseBurguer', 1.30], item6[105, 'Refrigerante', 1.0]]
-
-codigo = True
-while codigo != 0:
-    codigo = int(input("Digite o código do alimento: "))
-    if codigo in menu:
-        quantidade = int(input("Digite a quantidade que irá querer do alimento")
-    else:
-        print("Esse código não pertence a nenhum alimento.")
